ab08c352ac4bd018beec304e3dce4d0110e4be22
benjwu2 <benjwu2@gmail.com>
2023-07-31 22:27:25 -0400

    make empty definitions for all needed functions and add feedparser import

---
+++ autoPoster.py
@@ -0,0 +1,22 @@
+import feedparser
+
+
+# returns a feed object for the inputted RSS URL
+def returnFeed(url):
+    pass
+
+# returns a news article object
+def returnArticle():
+    pass
+
+# returns the link from the object for a news article
+def getLink():
+    pass
+
+# returns the constructed text that will be used to make a Facebook post for an article
+def returnPostContent():
+    pass
+
+# writes the constructed texts for all the news articles into a separate file
+def writePosts():
+    pass
