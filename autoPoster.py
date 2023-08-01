350eef527af7606c68204bbf67e0f1d89632e7f5
benjwu2 <benjwu2@gmail.com>
2023-08-01 07:22:01 -0400

    add returnFeedURLS function

--- autoPoster.py
+++ autoPoster.py
@@ -1,5 +1,14 @@
 import feedparser
 
+# returns a list of URLs for RSS feeds parsed from inputFeeds.txt
+def returnFeedURLs():
+    with open("inputFeeds.txt", "r") as file:
+        # stores the text content of inputFeeds.txt after the triple hyphens
+        unparsedList = file.read().split("---")[1]
+
+        # stores the individual entries in a list
+        entries = unparsedList.split("\n")
+
 
 # returns a feed object for the inputted RSS URL
 def returnFeed(url):
