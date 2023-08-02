32e58da918eb367bab84dd3451f1eb4be4eb38b3
benjwu2 <benjwu2@gmail.com>
2023-08-01 20:50:00 -0400

    add post function

--- autoPoster.py
+++ autoPoster.py
@@ -4,7 +4,7 @@ config = getattr(configFile, "config")
 
 pageURL = 'https://graph.facebook.com/{}/feed'.format(config["pageID"])
 
-# parse postsContent.txt for the list of post contents
+# parse postsContent.txt for the list of post contents and return a list of post contents
 def getPosts():
     with open("postsContent.txt", "r") as file:
         content = file.read()
@@ -13,4 +13,11 @@ def getPosts():
         return result
 
 
-print(getPosts()[9])
+
+# for each post in the list returned by getPosts, make a Facebook post
+def post():
+    posts = getPosts()
+    for post in posts:
+        # see configFile.py to edit the value of confi["pageAccessToken"]
+        payload = {"message": post, "token": config["pageAccessToken"]}
+        r = requests.post(pageURL, data=payload)
