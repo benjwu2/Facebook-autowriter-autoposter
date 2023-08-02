13e2bde6d3146ac1baeb54e591c5d6514c5168ae
benjwu2 <benjwu2@gmail.com>
2023-08-01 20:38:22 -0400

    create and test getPosts

--- autoPoster.py
+++ autoPoster.py
@@ -3,3 +3,14 @@ import configFile
 config = getattr(configFile, "config")
 
 pageURL = 'https://graph.facebook.com/{}/feed'.format(config["pageID"])
+
+# parse postsContent.txt for the list of post contents
+def getPosts():
+    with open("postsContent.txt", "r") as file:
+        content = file.read()
+
+        result = content.split("\n\n\n")
+        return result
+
+
+print(getPosts()[9])
